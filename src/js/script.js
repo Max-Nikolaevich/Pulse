3498ff0c0decb9462adef739920e156d6c6d8573
Max <example@gmail.com>
2021-11-26 07:30:43 +0500

    commit third

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+const { name } = require("browser-sync");
+
 $(document).ready(function(){
     $('.carousel__inner').slick( {
         speed: 1200,
@@ -15,20 +17,6 @@ $(document).ready(function(){
         ] 
         
     } );
-/* 
-    function toogleSlide(item) {
-        $('item').each(function(i){
-            $(this).on('click', function(e){
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $ ('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            })
-        }); 
-    };
-    toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back')
-  });
- */
   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -56,11 +44,44 @@ toggleSlide('.catalog-item__back');
     $('.modal__close').on('click', function() {
         $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
     });
+
     $('.button_mini').each(function(i) {
         $(this).on('click', function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
         })
-    })
+    });
+
+ 
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 });
 
